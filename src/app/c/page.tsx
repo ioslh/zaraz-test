d5760cd6eb6de9362608c694a987a71dea8123c1
ioslh <ohislh@gmail.com>
2025-03-27 10:02:22 +0800

    timeout

---
+++ src/app/c/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { trackEvent } from "@/utils/track";
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function C() {
+    useEffect(() => {
+        window.setTimeout(() => {
+            trackEvent('enter_c')
+        }, 100)
+    }, [])
+    return <div>
+        <h2 style={{fontSize: '20px', borderBottom: '1px solid blue'}}>C</h2>
+        <div>
+            <Link href="/d">To D</Link>
+        </div>
+    </div>
+}
---
+++ src/app/d/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { trackEvent } from "@/utils/track";
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function D() {
+    useEffect(() => {
+        window.setTimeout(() => {
+            trackEvent('enter_d')
+        }, 100)
+    }, [])
+    return <div>
+        <h2 style={{fontSize: '20px', borderBottom: '1px solid red'}}>D</h2>
+        <div>
+            <Link href="/c">To C</Link>
+        </div>
+    </div>
+}
